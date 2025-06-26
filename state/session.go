eef3e8a12c874b9cca230e7eda7d292bc61257f5
ulngollm <ulngollm@gmail.com>
2025-06-27 00:10:26 +0300

    fix race conditions, make HandleStep more straightforward

--- state/session.go
+++ state/session.go
@@ -1,10 +1,43 @@
 package state
 
+import "sync"
+
 //это единственная штука, которая меняется
 
 type Session struct {
-	UserID int64
-	State  State
-	Data   map[string]any
-	FlowID string
+	mu      sync.RWMutex
+	stateMu sync.RWMutex
+	state   State
+	Data    map[string]any
+	UserID  int64
+	FlowID  string
+}
+
+func NewSession(userID int64, flowID string, initialState State) *Session {
+	return &Session{UserID: userID, state: initialState, Data: make(map[string]any), FlowID: flowID}
+}
+
+func (s *Session) State() State {
+	s.stateMu.RLock()
+	defer s.stateMu.RUnlock()
+	return s.state
+}
+
+func (s *Session) SetState(State State) {
+	s.stateMu.Lock()
+	defer s.stateMu.Unlock()
+	s.state = State
+}
+
+func (s *Session) SetData(key string, value any) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.Data[key] = value
+}
+
+func (s *Session) GetData(key string) (any, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	val, ok := s.Data[key]
+	return val, ok
 }
--- state/store.go
+++ state/store.go
@@ -1,14 +1,22 @@
 package state
 
+import "sync"
+
 type Store struct {
+	mu   sync.RWMutex
 	pool map[int64]*Session
 }
 
 func NewStore() *Store {
-	return &Store{pool: make(map[int64]*Session)}
+	return &Store{
+		pool: make(map[int64]*Session),
+	}
 }
 
 func (s *Store) Get(userID int64) *Session {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	if session, ok := s.pool[userID]; ok {
 		return session
 	}
@@ -16,10 +24,16 @@ func (s *Store) Get(userID int64) *Session {
 }
 
 func (s *Store) Delete(session *Session) {
-	s.pool[session.UserID] = nil
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	delete(s.pool, session.UserID)
 }
 
 func (s *Store) Save(userID int64, session *Session) *Session {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.pool[userID] = session
 	return session
 }
