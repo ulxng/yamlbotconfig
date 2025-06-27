9c2c842900f7062857aaa9a59d9fffdc1054b472
ulngollm <ulngollm@gmail.com>
2025-06-27 18:49:59 +0300

    refactor loaders

--- lib/flow/loader.go
+++ lib/flow/loader.go
@@ -2,58 +2,45 @@ package flow
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+	"ulxng/blueprintbot/lib/config"
 
 	"gopkg.in/yaml.v3"
 )
+
+// для проверки на этапе компиляции
+var _ config.Loader[Flow] = (*Loader)(nil)
 
 type Loader struct {
 	Flows map[string]Flow
 }
 
 func NewLoader(source string) (*Loader, error) {
-	l := Loader{Flows: make(map[string]Flow)}
-	err := l.loadYamlFiles(source)
-	if err != nil {
-		return nil, fmt.Errorf("loadYamlFiles: %w", err)
+	l := &Loader{Flows: make(map[string]Flow)}
+	if err := l.Load(source); err != nil {
+		return nil, fmt.Errorf("load: %w", err)
 	}
-	return &l, nil
+	return l, nil
+}
+
+func (l *Loader) Load(path string) error {
+	return config.LoadYamlFiles(path, l.ParseData)
 }
 
 func (l *Loader) GetByKey(key string) Flow {
 	return l.Flows[key]
 }
 
-func (t *Loader) loadYamlFiles(path string) error {
-	return filepath.WalkDir(path, func(path string, d os.DirEntry, err error) error {
-		if err != nil {
-			return fmt.Errorf("walkDir: %w", err)
-		}
-		if d.IsDir() || !strings.HasSuffix(strings.ToLower(d.Name()), ".yaml") {
-			return nil
-		}
+func (l *Loader) ParseData(data []byte, path string) error {
+	var parsed map[string]Flow
+	if err := yaml.Unmarshal(data, &parsed); err != nil {
+		return fmt.Errorf("parse %s: %w", path, err)
+	}
 
-		data, err := os.ReadFile(path)
-		if err != nil {
-			return fmt.Errorf("read %s: %w", path, err)
+	for k, v := range parsed {
+		if _, exists := l.Flows[k]; exists {
+			return fmt.Errorf("duplicate key: %s in file %s", k, path)
 		}
-
-		//todo тут немного другой механизм парсинга нужен
-		//верхний уровень - flow. Под ним
-		var parsed map[string]Flow
-		if err := yaml.Unmarshal(data, &parsed); err != nil {
-			return fmt.Errorf("parse %s: %w", path, err)
-		}
-
-		for k, v := range parsed {
-			if _, exists := t.Flows[k]; exists {
-				return fmt.Errorf("duplicate key: %s in file %s", k, path)
-			}
-			t.Flows[k] = v
-		}
-
-		return nil
-	})
+		l.Flows[k] = v
+	}
+	return nil
 }
--- lib/messages/loader.go
+++ lib/messages/loader.go
@@ -2,56 +2,46 @@ package messages
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+	"ulxng/blueprintbot/lib/config"
 
 	"gopkg.in/yaml.v3"
 )
+
+var _ config.Loader[Message] = (*Loader)(nil)
 
 type Loader struct {
 	data map[string]Message
 }
 
 func NewLoader(source string) (*Loader, error) {
-	l := Loader{data: make(map[string]Message)}
-	err := l.loadYamlFiles(source)
-	if err != nil {
-		return nil, fmt.Errorf("loadYamlFiles: %w", err)
+	l := &Loader{data: make(map[string]Message)}
+
+	if err := l.Load(source); err != nil {
+		return nil, fmt.Errorf("load: %w", err)
 	}
-	return &l, nil
+	return l, nil
+}
+
+func (l *Loader) Load(path string) error {
+	return config.LoadYamlFiles(path, l.ParseData)
 }
 
 func (l *Loader) GetByKey(key string) Message {
 	return l.data[key]
 }
 
-func (t *Loader) loadYamlFiles(path string) error {
-	return filepath.WalkDir(path, func(path string, d os.DirEntry, err error) error {
-		if err != nil {
-			return fmt.Errorf("walkDir: %w", err)
-		}
-		if d.IsDir() || !strings.HasSuffix(strings.ToLower(d.Name()), ".yaml") {
-			return nil
-		}
+func (l *Loader) ParseData(data []byte, path string) error {
+	var parsed map[string]Message
+	if err := yaml.Unmarshal(data, &parsed); err != nil {
+		return fmt.Errorf("yaml.Unmarshal %s: %w", path, err)
+	}
 
-		data, err := os.ReadFile(path)
-		if err != nil {
-			return fmt.Errorf("os.ReadFile %s: %w", path, err)
+	for k, v := range parsed {
+		if _, exists := l.data[k]; exists {
+			return fmt.Errorf("duplicate key: %s in file %s", k, path)
 		}
+		l.data[k] = v
+	}
 
-		var parsed map[string]Message
-		if err := yaml.Unmarshal(data, &parsed); err != nil {
-			return fmt.Errorf("yaml.Unmarshal %s: %w", path, err)
-		}
-
-		for k, v := range parsed {
-			if _, exists := t.data[k]; exists {
-				return fmt.Errorf("duplicate key: %s in file %s", k, path)
-			}
-			t.data[k] = v
-		}
-
-		return nil
-	})
+	return nil
 }
