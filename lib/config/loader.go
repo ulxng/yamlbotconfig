9db83f6ea94609f87a079c52bd385157a674309a
ulngollm <ulngollm@gmail.com>
2025-06-27 18:49:59 +0300

    refactor loaders

---
+++ lib/config/loader.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type Loader[T any] interface {
+	Load(path string) error
+	GetByKey(key string) T
+}
+
+type ParseFunc func(data []byte, path string) error
+
+func LoadYamlFiles(path string, parseFunc ParseFunc) error {
+	return filepath.WalkDir(path, func(filePath string, d os.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("walkDir: %w", err)
+		}
+		if d.IsDir() || !strings.HasSuffix(strings.ToLower(d.Name()), ".yaml") {
+			return nil
+		}
+
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			return fmt.Errorf("os.ReadFile %s: %w", filePath, err)
+		}
+
+		return parseFunc(data, filePath)
+	})
+}
